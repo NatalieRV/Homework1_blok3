e1efa536540faa6bc5d2a84c04dee8e1fbfa538f
Natalie R <tatysia@gmail.com>
2023-01-25 09:31:54 +0300

    Задача о четности числа

---
+++ Ex3/Program.cs
@@ -0,0 +1,32 @@
+﻿int N = 4;
+int ost1 = N%2;
+if (ost1 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+int N2 = -3;
+int ost2 = N2%2;
+if (ost2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+int N3 = 7;
+int ost3 = N3%2;
+if (ost3 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
