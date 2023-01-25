cfd2f4277e86abbaa380fbec814f098fa2327cba
Natalie R <tatysia@gmail.com>
2023-01-25 09:03:46 +0300

    Задача 2 о максимуме трех чисел

---
+++ Ex2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Даны три числа. На экран вывести максимальное из них.
+// Программа выводит три примера последовательно.
+
+int A = 2;
+int B = 3;
+int C = 7;
+int max = A;
+if (A > max)
+{
+    max = A;
+}
+if (B > max)
+{
+    max = B;
+}
+if (C > max)
+{
+    max = C;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+
+int D = 44;
+int E = 5;
+int F = 78;
+int max2 = D;
+if (D > max2)
+{
+    max2 = D;
+}
+if (E > max2)
+{
+    max2 = E;
+}
+if (F > max2)
+{
+    max2 = F;
+}
+Console.Write("max2 = ");
+Console.WriteLine(max2);
+
+int G = 22;
+int H = 3;
+int I = 9;
+int max3 = G;
+if (G > max3)
+{
+    max3 = G;
+}
+if (H > max3)
+{
+    max3 = H;
+}
+if (I > max3)
+{
+    max3 = I;
+}
+Console.Write("max3 = ");
+Console.WriteLine(max3);
