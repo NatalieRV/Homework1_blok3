2bf8bb15e3079c0c0fc14f8d62c12230f8a31b0b
Natalie R <tatysia@gmail.com>
2023-01-25 09:53:44 +0300

    Задача о выводе четных чисел до N

---
+++ Ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Вывести все четные числа до N
+
+int N = 5;
+int K = N/2;
+int i = 1;
+Console.WriteLine("Пример 1");
+while (i <= K)
+{
+    Console.WriteLine(i*2);
+    i = i+1;
+}
+
+int N2 = 8;
+int K2 = N2/2;
+int i2 = 1;
+Console.WriteLine("Пример 2");
+while (i2 <= K2)
+{
+    Console.WriteLine(i2*2);
+    i2 = i2+1;
+}
