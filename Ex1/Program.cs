9a0a4e4739053b8fd9ac13965a4dab138dd6843a
Natalie R <tatysia@gmail.com>
2023-01-25 08:45:42 +0300

    Задача о максимуме из двух чисел

---
+++ Ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Ввести два числа, вывести максимальное из них. 
+// Три примера собраны в одну программу, последовательно.
+
+int A = 5;
+int B = 7;
+int max = A;
+if (A > max)
+{
+    max = A;
+}
+if (B > max)
+{
+ max = B;   
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+
+int C = 2;
+int D = 10;
+int max2 = C;
+if (C > max2)
+{
+    max2 = C;
+}
+if (D > max2)
+{
+ max2 = D;   
+}
+Console.Write("max2 = ");
+Console.WriteLine(max2);
+
+int E = -9;
+int F = -3;
+int max3 = E;
+if (E > max3)
+{
+    max3 = E;
+}
+if (F > max3)
+{
+ max3 = F;   
+}
+Console.Write("max3 = ");
+Console.WriteLine(max3);
